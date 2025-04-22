a7852d2a83437edf952e01c5627df66187ff9f38
pfranciscorojas <franciscorojas92@gmail.com>
2025-04-22 08:15:07 -0500

    add singleton to bd

---
+++ src/main/java/com/example/utils/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package com.example.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    
+    private static String url = "jdbc:mysql://localhost:3306";
+    private static String user = "root";
+    private static String pass = "root";
+
+    private static Connection myConn;
+
+    public static Connection getInstance()throws SQLException{
+        if(myConn != null){
+            myConn = DriverManager.getConnection(url, user,pass);
+        }
+        return myConn;
+    }
+
+
+}
